539638857cbb2be8bf4f12abeaed53db2dc92886
GEONAS STUNF AGUSTIN <48860973@ort>
2024-08-06 09:17:21 -0300

    database

---
+++ backend/DB/DBcontroller.js
@@ -0,0 +1,16 @@
+import chalk from 'chalk';
+import Sequelize from 'sequelize'
+
+const sequelize = new Sequelize ('campus_alumnos_db', 'root', 'rootroot',{
+    host: "127.0.0.1",
+    dialect: "mysql",
+    port: 3306
+})
+
+sequilize.authenticate()
+    .then(()=> {
+        console.log("BLABALA");
+
+    }).catch(err => {
+        console.error(err);
+    })
