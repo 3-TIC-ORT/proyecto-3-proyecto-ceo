3dc711e3aa7985035baa4cb49496daca4de1e6c6
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2024-08-06 14:07:17 -0300

    organizacion

--- backend/main.js
+++ backend/main.js
@@ -1,15 +1,14 @@
 /*cuandoexporta una variable tenes que importarla tambien 
 
 
-    aarchivo pepe
+    aarchivo pepePepon
     defino pepe
     export {pepe}
 
 
     archivo main
-    import {pepe} from '/backend/DB/DBcontroller.js'
+    import {pepe} from '/pepePepon'
     ahora puedo usar pepe
 
 
     */
-   
