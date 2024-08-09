3a2f91e88ffb294a2e62ae6a63607e5d7852ad93
loucimf <145812460+loucimf@users.noreply.github.com>
2024-08-09 10:39:04 -0300

    Resumen & Feedback models

---
+++ backend/model/feedback.js
@@ -0,0 +1,27 @@
+import Sequelize from "sequelize";
+import { sequelize } from "../DB/DBcontroller.js";
+import chalk from "chalk";
+import { type } from "os";
+
+const greenChalk = chalk.greenBright;
+const redChalk = chalk.redBright;
+const yellowChalk = chalk.yellowBright;
+const blueChalk = chalk.cyanBright;
+
+console.log(blueChalk("Definiendo modelo feedback..."));
+const FeedbackModel = sequelize.define('Feedback', {
+    puntaje: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+    },
+    sugerencia: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    opinion: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+})
+
+export { FeedbackModel }
---
+++ backend/model/resumenes.js
@@ -0,0 +1,35 @@
+import Sequelize from "sequelize";
+import { sequelize } from "../DB/DBcontroller.js";
+import chalk from "chalk";
+import { type } from "os";
+
+const greenChalk = chalk.greenBright;
+const redChalk = chalk.redBright;
+const yellowChalk = chalk.yellowBright;
+const blueChalk = chalk.cyanBright;
+
+console.log(blueChalk("Definiendo modelo resumen..."));
+const Resumen = sequelize.define('Resumen', {
+    archivo: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    contenido: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    titulo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    descripcion: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    filtros: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+})
+
+export { Resumen }
