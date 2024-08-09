83426ac8552cba446326c224bc4c0389959eb12b
loucimf <145812460+loucimf@users.noreply.github.com>
2024-08-09 15:39:35 -0300

    Connecting frontend-backend

--- backend/main.js
+++ backend/main.js
@@ -2,6 +2,8 @@ import { config } from 'dotenv';
 config()
 
 import  Express  from "express";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // seguridad
 import argon2 from 'argon2'
@@ -24,12 +26,19 @@ import { objPerdidosRouter } from './routes/objPerdidosRoute.js';
 import chalk from "chalk";
 import { error } from "console";
 
+//paths
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __parentDir = path.dirname(__dirname);
+
 const SECRET_KEY = process.env.SECRET_KEY
 
 const greenChalk = chalk.greenBright;
 const redChalk = chalk.redBright;
 const yellowChalk = chalk.yellowBright;
 const blueChalk = chalk.cyanBright;
+
+const __fileName = 
 
 console.log("0-----------------------S-T-A-R-T-I-N-G-----------------------0")
 const app = Express()
@@ -177,5 +186,5 @@ async function authenticateToken(req, res, next) {
 }   
 
 
-
+export { app }
 export { authenticateToken };
--- backend/routes/campusRouter.js
+++ backend/routes/campusRouter.js
@@ -1,7 +1,19 @@
 import express from 'express'
 import { Router } from 'express'
+import { app } from '../main.js'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
 const campusRouter = Router()
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __parentDir = path.dirname(__dirname);
+const __rootDir = path.dirname(__parentDir);
+
+campusRouter.get('/', (req, res) => {
+    app.use(express.static(path.join(__rootDir, 'frontend')));
+    res.sendFile(path.join(__rootDir, 'frontend/LogIn/LogIn.html'))
+})
 
 export { campusRouter }
