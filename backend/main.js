76a6e95c407ddf7dcafa9d19eb01116f42d00b6f
loucimf <145812460+loucimf@users.noreply.github.com>
2024-08-09 09:40:42 -0300

    Update Data handling
    Encripting contraseñas y verifying

--- backend/main.js
+++ backend/main.js
@@ -1,23 +1,41 @@
 import  Express  from "express";
 import  express  from "express";
 import { User } from "./model/users.js";
+import argon2 from 'argon2'
+import jsonwebtoken from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+//colores
+import chalk from "chalk";
+
+const greenChalk = chalk.greenBright;
+const redChalk = chalk.redBright;
+const yellowChalk = chalk.yellowBright;
+const blueChalk = chalk.cyanBright;
 
 console.log("--------------------------------------------------------------------------------------------")
-
 const app = Express()
 const PORT = 3000
 
 app.post('/send_data', async (req, res) => {
+<<<<<<< Updated upstream
     const userData = req.body; 
 
     console.log("Recibiendo data...")
+=======
+
+    const userData = req.body;
+
+    console.log("Recibiendo data...");
+>>>>>>> Stashed changes
 
     let firstName = userData.firstName;
-    let password = userData.password;
+    let password = await encriptPassword(userData.password)
     let lastName = userData.lastName;
     let gmail = userData.gmail;
 
     const user = await User.create({firstName: firstName, lastName: lastName, password: password, gmail: gmail})
+<<<<<<< Updated upstream
     
 })
 
@@ -38,4 +56,31 @@ app.listen(PORT, () => {
         
     }
 })
+=======
+
+})
+
+
+async function encriptPassword(password) {
+    try {
+        let hash = argon2.hash(password, 5)
+        return hash;
+    } catch (error) {
+        console.error(err, "ERROR =(")
+    }
+}
+
+async function verifyPassword(hash, password) {
+    try {
+        if ( await argon2.verify(hash, password)) {
+            return true
+        } else {
+            return false
+        }
+    } catch (error) {
+        console.err(err, "ERROR, no hizo la verificacion")
+    }
+}
+
+>>>>>>> Stashed changes
 
