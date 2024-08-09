1a0548bf942ea112512c90393dd29df994c65440
loucimf <145812460+loucimf@users.noreply.github.com>
2024-08-09 10:04:09 -0300

    Embelleciendo la obra maestra
    (puse colorsitos)

--- backend/model/users.js
+++ backend/model/users.js
@@ -1,8 +1,14 @@
 import { timeStamp } from "console";
 import Sequelize from "sequelize";
 import { sequelize } from "../DB/DBcontroller.js";
+import chalk from "chalk";
 
-console.log("Definiendo modelo usuario...")
+const greenChalk = chalk.greenBright;
+const redChalk = chalk.redBright;
+const yellowChalk = chalk.yellowBright;
+const blueChalk = chalk.cyanBright;
+
+console.log(blueChalk("Definiendo modelo usuario..."));
 const User = sequelize.define('Users', {
     password: {
         type: Sequelize.STRING,
