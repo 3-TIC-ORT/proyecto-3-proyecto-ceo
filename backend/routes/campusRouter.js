ff564807e9281b27058098338737ba755d14ae16
loucimf <145812460+loucimf@users.noreply.github.com>
2024-08-09 15:17:05 -0300

    Added routes handling

--- backend/routes/campusRouter.js
+++ backend/routes/campusRouter.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { Router } from 'express'
 
-const objPerdidosRouter = Router()
+const campusRouter = Router()
 
 
-export { objPerdidosRouter }
+export { campusRouter }
