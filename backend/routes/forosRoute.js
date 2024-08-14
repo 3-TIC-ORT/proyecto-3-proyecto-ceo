4c255ba9acc2a1ab8489ae3642dae8b824b70165
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2024-08-14 15:31:08 -0300

    Big error, and others solution

--- backend/routes/forosRoute.js
+++ backend/routes/forosRoute.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import { Router } from 'express'
+import { app } from '../main.js'
+import { fileURLToPath } from 'url'
 import path from 'path';
 
 const forosRouter = Router()
--- backend/routes/objPerdidosRoute.js
+++ backend/routes/objPerdidosRoute.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import { Router } from 'express'
+import { app } from '../main.js'
+import { fileURLToPath } from 'url'
+import path from 'path';
 
 const objPerdidosRouter = Router();
 
@@ -8,7 +11,7 @@ const __dirname = path.dirname(__filename);
 const __parentDir = path.dirname(__dirname);
 const __rootDir = path.dirname(__parentDir);
 
-objPerdidosRouter.get('/objetosPerdidos', (req, res)=>{
+objPerdidosRouter.get('/', (req, res)=>{
     app.use(express.static(path.join(__rootDir, 'frontend/ObjetosPerdidos')));
     res.sendFile(path.join(__rootDir, 'frontend/ObjetosPerdidos/ObjetosPerdidos.html'));
 })
