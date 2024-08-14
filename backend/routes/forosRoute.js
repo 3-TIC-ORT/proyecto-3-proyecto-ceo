55c0b3d49ff1ab02812668930395b517537e130a
loucimf <145812460+loucimf@users.noreply.github.com>
2024-08-14 10:29:17 -0300

    Handling routes (Foros & Campus)

--- backend/routes/forosRoute.js
+++ backend/routes/forosRoute.js
@@ -3,4 +3,33 @@ import { Router } from 'express'
 
 const forosRouter = Router()
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __parentDir = path.dirname(__dirname);
+const __rootDir = path.dirname(__parentDir);
+
+forosRouter.get("/", (req, res) => {
+
+    app.use(express.static(path.join(__rootDir, 'frontend/Foro')));
+    res.sendFile(path.join(__rootDir, 'frontend/Foro/Foro.html'))
+})
+
+forosRouter.get("/search", (req, res) => {
+
+    app.use(express.static(path.join(__rootDir, 'frontend/ForoBusqueda')));
+    res.sendFile(path.join(__rootDir, 'frontend/ForoBusqueda/ForoBusqueda.html'))
+})
+
+forosRouter.get("/upload", (req, res) => {
+
+    app.use(express.static(path.join(__rootDir, 'frontend/ForoUpload')));
+    res.sendFile(path.join(__rootDir, 'frontend/ForoUpload/ForoUpload.html'))
+})
+
+forosRouter.get("/open", (req, res) => {
+
+    app.use(express.static(path.join(__rootDir, 'frontend/ForoVisualizacion')));
+    res.sendFile(path.join(__rootDir, 'frontend/ForoVisualizacion/ForoVisualizacion.html'))
+})
+
 export { forosRouter }
