f0797bd060043073c10a3d99098099662bbc2be9
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2024-09-04 14:30:00 -0300

    error fixed

--- backend/endpoints.js
+++ backend/endpoints.js
@@ -1,17 +1,32 @@
-import argon2 from 'argon2'
+import argon2 from 'argon2';
+import jsonwebtoken from 'jsonwebtoken';
+import multer from 'multer';
+import path from 'path';
 import { User } from './model/users.js';
 import { Foro } from './model/foros.js';
 import { Resumen } from './model/resumenes.js';
 import { objetosPerdidos } from './model/objetosPerdidos.js';
 import { FeedbackModel } from './model/feedback.js';
 import { Intercambio } from './model/intercambio.js';
-import { Console, error } from 'console';
-import multer from 'multer';
-import path from 'path';
+import express from 'express';
+import bodyParser from 'body-parser';
+import chalk from 'chalk';
 
+const greenChalk = chalk.greenBright;
+const redChalk = chalk.redBright;
+const yellowChalk = chalk.yellowBright;
+const blueChalk = chalk.cyanBright;
+
+const app = express();
+
+app.use(bodyParser.json());
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
+        const uploadPath = path.join(__dirname, 'uploads');
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath);
+        }
         cb(null, 'uploads/');
     },
     filename: (req, file, cb) => {
@@ -19,164 +34,165 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
     }
 });
+const upload = multer({ storage });
 
-// functiones
 async function encriptPassword(password) {
     try {
-        let hash = argon2.hash(password, 5)
-        console.log('encripted-password')
+        let hash = await argon2.hash(password, { type: argon2.argon2id, saltLength: 16 });
+        console.log('Password encrypted successfully');
         return hash;
     } catch (error) {
-        console.error(error, "ERROR =(")
+        console.error("Error encrypting password:", error);
     }
 }
 
 async function verifyPassword(hash, password) {
     try {
-        if ( await argon2.verify(hash, password)) {
-            return true
+        if (await argon2.verify(hash, password)) {
+            return true;
         } else {
-            return false
+            return false;
         }
     } catch (error) {
-        console.err(error, "ERROR, no hizo la verificacion")
+        console.error("Error verifying password:", error);
     }
 }
 
 async function authenticateToken(req, res, next) {
-    // sacamo el token del header
-    const authHeader = req.header['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
-    console.log(blueChalk('Authenticating token...'))
+    console.log('Authenticating token...');
 
     if (token == null) {
-        console.log(redChalk('No token!'))
-        return error;
+        console.log('No token!');
+        return res.sendStatus(401);
     }
 
     jsonwebtoken.verify(token, SECRET_KEY, (err, user) => {
-
-        console.log(yellowChalk('Authenticating token....'))
         if (err) {
-            console.log(redChalk('Invalid token!'))
+            console.log('Invalid token!');
             return res.sendStatus(403);
         }
 
-        console.log(greenChalk('Authentication successful!!!!'))
+        console.log('Authentication successful!');
         req.user = user;
         next();
+    });
+}
 
-    })
-}   
-
-// endpoitns
 export async function endpoints(app) {
     app.post('/send-register', async (req, res) => {
         try {
             const userData = req.body;
-            console.log("Recibiendo user data...");
+            console.log("Receiving user data...");
 
             let firstName = userData.firstName;
-            let password = await encriptPassword("pepe");
+            let password = await encriptPassword(userData.password);
             let lastName = userData.lastName;
             let gmail = userData.gmail;
         
-            const user = await User.create({firstName: firstName, lastName: lastName, password: password, gmail: gmail});
-            res.status(200).json({ message: 'Usuario creado exitosamente'});
+            const user = await User.create({ firstName: firstName, lastName: lastName, password: password, gmail: gmail });
+            res.status(200).json({ message: 'User created successfully' });
         } catch (error) {
-            console.error("Error al crear el usuario:", error);
-            res.status(500).json({ message: 'Error al crear el usuario', error });
+            console.error("Error creating user:", error);
+            res.status(500).json({ message: 'Error creating user', error });
         }
     });
 
     app.post('/send-intercambio', upload.single('foto'), async (req, res) => {
-        try{
-            const intercambioData = req.body
-            console.log("Recibiendo intecambio data");
+        try {
+            const intercambioData = req.body;
+            console.log("Receiving intercambio data");
 
             let informacion = intercambioData.informacion;
-            let titulo = intercambioData.titulo;
-            let respuestas = intercambioData.respuestas;
+            let titulo = intercambioData.titulo
+            let respuestas = intercambioData.respuestas
             let foto = req.file ? req.file.filename : null;
 
-            const intecambio = await Intercambio.create({informacion: informacion, titulo: titulo, respuestas: respuestas, foto:foto});
-            res.status(200).json({ message: 'Intercambio creado exitosamente'});
-        } catch(error) {
-            console.error("Error al crear el usuario:", error);
-            res.status(500).json({ message: 'Error al crear el usuario', error });
+            const intercambio = await Intercambio.create({ informacion: informacion, titulo: titulo, respuestas: respuestas, foto: foto });
+            res.status(200).json({ message: 'Intercambio created successfully' });
+        } catch (error) {
+            console.error("Error creating intercambio:", error);
+            res.status(500).json({ message: 'Error creating intercambio', error });
         }
     });
 
     app.post('/send-feedback', async (req, res) => {
         try {
             const feedbackData = req.body;
-            console.log("Recibiendo feedback data...");
+            console.log("Receiving feedback data...");
 
-            let score = feedbackData.puntaje;
-            let suggestion = feedbackData.sugerencia;
+            let puntaje = feedbackData.puntaje;
+            let sugerencia = feedbackData.sugerencia;
             let opinion = feedbackData.opinion;
         
-            const feedback = await FeedbackModel.create({puntaje: score, sugerencia: suggestion, opinion: opinion});
-            res.status(201).json({ message: 'Feedback enviado exitosamente'});
+            const feedback = await FeedbackModel.create({ puntaje: puntaje, sugerencia: sugerencia, opinion: opinion });
+            res.status(201).json({ message: 'Feedback sent successfully' });
         } catch (error) {
-            console.error("Error al enviar el feedback:", error);
-            res.status(500).json({ message: 'Error al enviar el feedback', error });
+            console.error("Error sending feedback:", error);
+            res.status(500).json({ message: 'Error sending feedback', error });
         }
     });
 
     app.post('/send-resumen', upload.single('archivo'), async (req, res) => {
         try {
             const resumenData = req.body;
-            console.log("Recibiendo resumen data...");
+            console.log("Receiving resumen data...");
 
             let descripcion = resumenData.descripcion;
-            let titulo = resumenData.titulo;
-            let archivo = req.file ? req.file.filename : null;
             let contenido = resumenData.contenido;
+            let titulo = resumenData.titulo;
             let filtros = resumenData.filtros;
+            let archivo = req.file ? req.file.filename : null;
         
-            const resumen = await Resumen.create({titulo: titulo, descripcion: descripcion, archivo: archivo, contenido: contenido, filtros: filtros});
-            res.status(201).json({ message: 'Resumen enviado exitosamente'});
+            const resumen = await Resumen.create({ titulo: titulo, descripcion: descripcion, archivo: archivo, contenido: contenido, filtros: filtros });
+            res.status(201).json({ message: 'Resumen sent successfully' });
         } catch (error) {
-            console.error("Error al enviar el resumen:", error);
-            res.status(500).json({ message: 'Error al enviar el resumen', error });
+            console.error("Error sending resumen:", error);
+            res.status(500).json({ message: 'Error sending resumen', error });
         }
     });
 
     app.post('/send-objetosPerdidos', upload.single('foto'), async (req, res) => {
         try {
             const objetosPerdidosData = req.body;
-            console.log("Recibiendo objetosPerdidos data...");
+            console.log("Receiving objetosPerdidos data...");
 
             let informacion = objetosPerdidosData.informacion;
             let foto = req.file ? req.file.filename : null;
         
-            const objeto = await objetosPerdidos.create({informacion: informacion, foto: foto});
-            res.status(201).json({ message: 'Objeto perdido registrado exitosamente'});
+            const objeto = await objetosPerdidos.create({ informacion: informacion, foto: foto });
+            res.status(201).json({ message: 'Objeto perdido registered successfully' });
         } catch (error) {
-            console.error("Error al registrar el objeto perdido:", error);
-            res.status(500).json({ message: 'Error al registrar el objeto perdido', error });
+            console.error("Error registering objeto perdido:", error);
+            res.status(500).json({ message: 'Error registering objeto perdido', error });
         }
     });
 
     app.post('/send-foro', upload.single('foto'), async (req, res) => {
         try {
             const foroData = req.body;
-            console.log("Recibiendo foro data...");
+            console.log("Receiving foro data...");
 
             let pregunta = foroData.pregunta;
-            let foto = req.file ? req.file.filename : null;
             let textoExplicativo = foroData.textoExplicativo;
             let comentarios = foroData.comentarios;
+            let foto = req.file ? req.file.filename : null;
         
-            const foro = await Foro.create({pregunta: pregunta, foto: foto, textoExplicativo: textoExplicativo, comentarios: comentarios});
-            res.status(201).json({ message: 'Foro creado exitosamente'});
+            const foro = await Foro.create({ pregunta: pregunta, foto: foto, textoExplicativo: textoExplicativo, comentarios: comentarios });
+            res.status(201).json({ message: 'Foro created successfully' });
         } catch (error) {
-            console.error("Error al crear el foro:", error);
-            res.status(500).json({ message: 'Error al crear el foro', error });
+            console.error("Error creating foro:", error);
+            res.status(500).json({ message: 'Error creating foro', error });
         }
     });
 }
 
-export {  authenticateToken }
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(redChalk(`Server is running on port ${PORT}`));
+});
+
+export { authenticateToken };
